5a090a931adaf17fc3dad365dcd5c5296b22aafd
mechamogera <mechamosura@gmail.com>
2013-07-10 16:19:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# cookbook-cloudwatch-mon-put cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: mechamogera (<mechamosura@gmail.com>)
---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default["cloudwatch-mon-put"]["deploy-user"] = "/home/ec2-user/script"
+default["cloudwatch-mon-put"]["user"] = "ec2-user"
+default["cloudwatch-mon-put"]["group"] = "ec2-user"
